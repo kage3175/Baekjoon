1ced21257700034c84d5bfcc6493fddf1fcfbf72
hyunsoo <hjames2000@snu.ac.kr>
2022-11-12 15:17:45 +0900

    xv

---
+++ Baek12865.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int n;int k;
+int v[101]={0};
+int w[101]={0};
+int dp[101][100001];
+
+
+int main(void){
+	
+	scanf("%d %d", &n, &k);
+	for(int i=1;i<n+1;i++) scanf("%d %d", &w[i], &v[i]);
+	
+	
+	
+	printf("%d", max);
+	
+	return 0;
+}
+
